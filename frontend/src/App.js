234e8c013cd1fea2fe2f87050eb8bc40f64b7aa6
faizalajiw <faizalajiwibowo7@gmail.com>
2022-06-12 12:48:27 +0700

    menambahkan router login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
-import './App.css';
+// import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Navbars from "./components/Navbars";
+import Login from "./pages/Login";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">Halo React</div>
+    <BrowserRouter>
+      <Navbars />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element ={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
