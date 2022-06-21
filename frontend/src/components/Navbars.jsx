6768b4556d83953e54206c5850dda8d94cc607c2
faizalajiw <faizalajiwibowo7@gmail.com>
2022-06-21 20:45:33 +0700

    remove dropdown

--- frontend/src/components/Navbars.jsx
+++ frontend/src/components/Navbars.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
+import { Navbar, Nav, Container } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 
 const Navbars = () => {
