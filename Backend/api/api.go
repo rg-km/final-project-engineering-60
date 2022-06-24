88fb863a31a18ef73f04c44dc561873bc91d6d07
lismanurmalaa <lismanurmala76@gmail.com>
2022-06-24 18:58:45 +0700

    api-register

--- Backend/api/api.go
+++ Backend/api/api.go
@@ -18,6 +18,7 @@ func NewAPI(userRepo repository.UserRepository) API {
 		userRepo, mux,
 	}
 	mux.Handle("/api/user/login", api.POST(http.HandlerFunc(api.login)))
+	mux.Handle("/api/user/register", api.POST(http.HandlerFunc(api.register)))
 	mux.Handle("/api/user/logout", api.POST(http.HandlerFunc(api.logout)))
 	mux.Handle("/api/products", api.GET(http.HandlerFunc(api.userlist)))
 	return api
