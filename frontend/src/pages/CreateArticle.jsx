d4bbaf96fb476aea9f730b1fed81775148ee8aeb
Hafizulakbar <hafizul.ab@mhs.unsyiah.ac.id>
2022-06-27 01:57:33 +0700

    Merge branch 'main' of https://github.com/rg-km/final-project-engineering-60 into home

---
+++ frontend/src/pages/CreateArticle.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function createArticle () {
+    return(
+        <div className="container">
+            <form>
+                <div class="mb-3" style={{marginTop:"50px", boxShadow:"2px 2px #bdbdbd"}}>
+                    <input type="text" placeholder="Judul" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                    
+                </div>
+                <div class="mb-3" style={{ boxShadow:"2px 2px #bdbdbd"}}>
+                    <input type="text" placeholder="Category" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                    
+                </div>
+                <div class="input-group mb-3" style={{ boxShadow:"2px 2px #bdbdbd"}}>
+                <label class="input-group-text" for="inputGroupFile01">Upload</label>
+                <input type="file" class="form-control" id="inputGroupFile01"/>
+                </div>
+
+                <div class="mb-3" style={{boxShadow:"2px 2px #bdbdbd"}}>
+                <textarea class="form-control" placeholder="Create Article" id="exampleFormControlTextarea1" rows="10"></textarea>
+                </div>
+                    <button type="submit" class="btn btn-danger">Submit</button>
+            </form>
+        </div>
+    )
+}
