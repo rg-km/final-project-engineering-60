62dc126437ab50a696d9d621a2218991a0a966ff
faizalajiw <faizalajiwibowo7@gmail.com>
2022-06-27 00:49:49 +0700

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Navbars from "./components/Navbars";
+import Navbars from "./components/common/Navbar/Navbars";
+import Footer from "./components/common/Footer/Footer";
 import Card from "./pages/About/Card";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Footer from "./components/Footer";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import './App.css';
+import SingleBlog from './pages/SingleBlog';
 
 const App = () => {
   return (
@@ -12,9 +14,11 @@ const App = () => {
       <Navbars />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path='/blog/:id' element={<SingleBlog />} />
         <Route path="/about" element={<Card />} />
         <Route path="/login" element ={<Login />} />
         <Route path="/register" element ={<Register />} />
+        <Route to='/' />
       </Routes>
       <Footer />
     </BrowserRouter>
