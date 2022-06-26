0aff149050e5407229db3a260a4ae210f0e14332
faizalajiw <faizalajiwibowo7@gmail.com>
2022-06-27 00:22:26 +0700

    fix login & register layout

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,52 @@
-import React from "react";
-import Hero from "../components/Hero";
-
+import React, { useState } from "react";
+import Hero from "../components/common/Hero/Hero";
+import EmptyList from "../../src/components/common/EmptyList";
+import BlogList from "../../src/components/content/BlogList";
+import SearchBar from "../../src/components/content/SearchBar";
+import { blogList } from "../../src/components/config/data";
 
 const Home = () => {
-    return (
-        <div>
-            <Hero />
-            
-        </div>
+  const [blogs, setBlogs] = useState(blogList);
+  const [searchKey, setSearchKey] = useState("");
+
+  // Search submit
+  const handleSearchBar = (e) => {
+    e.preventDefault();
+    handleSearchResults();
+  };
+
+  // Search for blog by category
+  const handleSearchResults = () => {
+    const allBlogs = blogList;
+    const filteredBlogs = allBlogs.filter((blog) =>
+      blog.category.toLowerCase().includes(searchKey.toLowerCase().trim())
     );
+    setBlogs(filteredBlogs);
+  };
+
+  // Clear search and show all blogs
+  const handleClearSearch = () => {
+    setBlogs(blogList);
+    setSearchKey("");
+  };
+
+  return (
+    <div>
+      {/* Page Hero/Banner */}
+      <Hero />
+
+      {/* Search Bar */}
+      <SearchBar
+        value={searchKey}
+        clearSearch={handleClearSearch}
+        formSubmit={handleSearchBar}
+        handleSearchKey={(e) => setSearchKey(e.target.value)}
+      />
+
+      {/* Blog List & Empty View */}
+      {!blogs.length ? <EmptyList /> : <BlogList blogs={blogs} />}
+    </div>
+  );
 };
 
 export default Home;
